bb4847f72eca898888a4889329a00a7388051916
Junghwan Han <therealpizzaroot@gmail.com>
2023-05-13 05:46:16 +0900

    ms

--- Key Viewer.py
+++ Key Viewer.py
@@ -29,27 +29,54 @@ f = open('keymap6.txt')
 keymap6 = json.load(f)
 f.close()
 
+last = 0
+diff = 0
 velocity = float(open('velocity.txt', 'r').read())
 def keydown(i):
+    global last, diff
     if mode == 8:
         for j in range(8):
             if i.name.lower() == keymap8[j] and state8[j] == False:
                 state8[j] = True
+                if i.time - last > 2:
+                    diff = 0
+                    last = i.time
+                elif i.time - last > 0.02:
+                    diff = i.time - last
+                    last = i.time
                 events8[j].append(i.time)
     if mode == 4:
         for j in range(4):
             if i.name.lower() == keymap4[j] and state4[j] == False:
                 state4[j] = True
+                if i.time - last > 2:
+                    diff = 0
+                    last = i.time
+                elif i.time - last > 0.02:
+                    diff = i.time - last
+                    last = i.time
                 events4[j].append(i.time)
     if mode == 6:
         for j in range(6):
             if i.name.lower() == keymap6[j] and state6[j] == False:
                 state6[j] = True
+                if i.time - last > 2:
+                    diff = 0
+                    last = i.time
+                elif i.time - last > 0.02:
+                    diff = i.time - last
+                    last = i.time
                 events6[j].append(i.time)
     if mode == 5:
         for j in range(6):
             if i.name.lower() == keymap5[j] and state5[j] == False:
                 state5[j] = True
+                if i.time - last > 2:
+                    diff = 0
+                    last = i.time
+                elif i.time - last > 0.02:
+                    diff = i.time - last
+                    last = i.time
                 events5[j].append(i.time)
 
 def keyup(i):
@@ -115,7 +142,7 @@ def main():
      
     # create a surface on screen that has the size of 240 x 180
     screen = pygame.display.set_mode((504,330))
-     
+    font = pygame.font.SysFont("Arial", 30)
     # define a variable to control the main loop
     running = True
     
@@ -134,44 +161,46 @@ def main():
         if mode == 8:
             for i in range(2):
                 for j in range(0, len(events8[i + 6]), 2):
-                    pygame.draw.rect(screen, (255,0,0), (i * 252, (curtime - events8[i + 6][j]) * 180 * velocity, 252, 15))
+                    pygame.draw.rect(screen, (255,0,0), (i * 252, (curtime - events8[i + 6][j]) * 282 * velocity, 252, 15))
             for i in range(6):
                 for j in range(0, len(events8[i]), 2):
                     if i % 3 == 1:
-                        pygame.draw.rect(screen, (0,0,255), (i * 84, (curtime - events8[i][j]) * 180 * velocity, 84, 15))
+                        pygame.draw.rect(screen, (0,0,255), (i * 84, (curtime - events8[i][j]) * 282 * velocity, 84, 15))
                     else:
-                        pygame.draw.rect(screen, (255,255,255), (i * 84, (curtime - events8[i][j]) * 180 * velocity, 84, 15))
+                        pygame.draw.rect(screen, (255,255,255), (i * 84, (curtime - events8[i][j]) * 282 * velocity, 84, 15))
         elif mode == 6:
             for i in range(6):
                 for j in range(0, len(events6[i]), 2):
                     if i % 3 == 1:
-                        pygame.draw.rect(screen, (0,0,255), (i * 84, (curtime - events6[i][j]) * 180 * velocity, 84, 15))
+                        pygame.draw.rect(screen, (0,0,255), (i * 84, (curtime - events6[i][j]) * 282 * velocity, 84, 15))
                     else:
-                        pygame.draw.rect(screen, (255,255,255), (i * 84, (curtime - events6[i][j]) * 180 * velocity, 84, 15))
+                        pygame.draw.rect(screen, (255,255,255), (i * 84, (curtime - events6[i][j]) * 282 * velocity, 84, 15))
         elif mode == 4:
             for i in range(4):
                 for j in range(0, len(events4[i]), 2):
                     if i == 1 or i == 2:
-                        pygame.draw.rect(screen, (0,0,255), (i * 126, (curtime - events4[i][j]) * 180 * velocity, 126, 15))
+                        pygame.draw.rect(screen, (0,0,255), (i * 126, (curtime - events4[i][j]) * 282 * velocity, 126, 15))
                     else:
-                        pygame.draw.rect(screen, (255,255,255), (i * 126, (curtime - events4[i][j]) * 180 * velocity, 126, 15))
+                        pygame.draw.rect(screen, (255,255,255), (i * 126, (curtime - events4[i][j]) * 282 * velocity, 126, 15))
         elif mode == 5:
             for i in range(2):
                 for j in range(0, len(events5[i]), 2):
                     if i == 1:
-                        pygame.draw.rect(screen, (0,0,255), (i * 101, (curtime - events5[i][j]) * 180 * velocity, 101, 15))
+                        pygame.draw.rect(screen, (0,0,255), (i * 101, (curtime - events5[i][j]) * 282 * velocity, 101, 15))
                     else:
-                        pygame.draw.rect(screen, (255,255,255), (i * 101, (curtime - events5[i][j]) * 180 * velocity, 101, 15))
+                        pygame.draw.rect(screen, (255,255,255), (i * 101, (curtime - events5[i][j]) * 282 * velocity, 101, 15))
             for i in range(2):
                 for j in range(0, len(events5[i + 2]), 2):
-                    pygame.draw.rect(screen, (255,255,255), (i * 50 + 202, (curtime - events5[i + 2][j]) * 180 * velocity, 50, 15))
+                    pygame.draw.rect(screen, (255,255,255), (i * 50 + 202, (curtime - events5[i + 2][j]) * 282 * velocity, 50, 15))
             for i in range(2):
                 for j in range(0, len(events5[i + 4]), 2):
                     if i == 0:
-                        pygame.draw.rect(screen, (0,0,255), (i * 101 + 302, (curtime - events5[i + 4][j]) * 180 * velocity, 101, 15))
+                        pygame.draw.rect(screen, (0,0,255), (i * 101 + 302, (curtime - events5[i + 4][j]) * 282 * velocity, 101, 15))
                     else:
-                        pygame.draw.rect(screen, (255,255,255), (i * 101 + 302, (curtime - events5[i + 4][j]) * 180 * velocity, 101, 15))
+                        pygame.draw.rect(screen, (255,255,255), (i * 101 + 302, (curtime - events5[i + 4][j]) * 282 * velocity, 101, 15))
 
+        text = font.render(str(int(diff * 1000)) + " ms", True, (255, 255, 255))
+        screen.blit(text, [10, 10])
         pygame.display.update()
 
 main()
